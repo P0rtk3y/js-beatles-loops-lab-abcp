756ccf37a009bc5cff430a3261fb8bdf2aa4ce28
Lana Worobec <llama.p0rtk3y@gmail.com>
2018-01-19 08:02:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(arrayMusicians, arrayInstruments) {
+ var whoPlaysWhat = [];
+ for (let i = 0; i < arrayMusicians.length; i++) {
+     whoPlaysWhat.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+ }
+ return whoPlaysWhat;
+}
+
+function johnLennonFacts(facts){
+  var results = [];
+  var count = 0;
+  while (count < facts.length){
+    results.push(facts[count] + '!!!');
+    count++
+  }
+  return results;
+}
+
+function iLoveTheBeatles(num){
+  var results = [];
+  do {
+    results.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return results;
+}
